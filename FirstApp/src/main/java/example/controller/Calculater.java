bd754122bc3189df1bbba86666dc54a301d9c163
ib123 <ib123raheem@gmail.com>
2022-05-31 14:17:15 +0300

    first comit

--- FirstApp/src/main/java/example/controller/Calculater.java
+++ FirstApp/src/main/java/example/controller/Calculater.java
@@ -2,14 +2,32 @@ package example.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Set;
+
+import javax.naming.*;
 
 import example.model.CalcModel;
+import example.model.EmployeeModel;
+import example.model.RoleModel;
+import example.model.TaskModel;
+import example.model.TeamModel;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import javax.sql.*;
+
+import dao.EmployeeDao;
+import dao.TaskDao;
+
+import java.sql.*;
 
 
 
@@ -29,13 +47,129 @@ public class Calculater extends HttpServlet {
 	 * 
 	 * /FirstApp/src/main/java/example/controller/Calculater.java
 	 */
-	private CalcModel calcModel=new CalcModel();
+	
 	private static final long serialVersionUID = 1L;
 	
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		PrintWriter out = response.getWriter();
+		RoleModel role = new RoleModel();
+		role.setId(1);
+		/*EmployeeModel employee=new EmployeeModel("ibraheem", "Arda","Arda@Hotmail.com",
+				 "123456", "ard11",
+				role);
+		EmployeeDao ee= new EmployeeDao();
+		ee.addEmployee(employee);*/
+		/*Set<EmployeeModel> employees;
+		 * 
+		 * 
+		 */
+		TaskModel task= new TaskModel( "test2","done", "22.txt");
+		TaskDao taskDao=new TaskDao();
+		EmployeeDao ee= new EmployeeDao();
+		
+		try {
+			
+			
+			//EmployeeModel emplo=ee.getByUserName("ali");
+			//out.append("<div>"+emplo.toString()+"</div>");
+			//taskDao.addTask(task);
+			//taskDao.assignTaskToEmployee(4, "ali");
+			TaskModel getTask=taskDao.getById(3);
+			Set<TaskModel> byEmployee=taskDao.getTaskByEmployee("ali");
+			Set<TaskModel> taskDone=taskDao.getTaskDone();
+			Set<TaskModel> byEmployeeDone=taskDao.getTaskDoneByEmployee("ali");
+			Set<TaskModel> byEmployeeProgress=taskDao.getTaskInProgressByEmployee("ali");
+			Set<TaskModel> inProgress=taskDao.getTaskInProgress();
+			Set<TaskModel> all=taskDao.getTasks();
+			//employees = ee.getTeam(2);
+			for(TaskModel taskModel:byEmployee) {
+				
+				out.append("<div> byEmployee:::"+taskModel.toString()+"</div>");
+			}
+			
+for(TaskModel taskModel:taskDone) {
+				
+				out.append("<div> taskDone:::"+taskModel.toString()+"</div>");
+			}
+for(TaskModel taskModel:byEmployeeDone) {
 	
+	out.append("<div> byEmployeeDone:::"+taskModel.toString()+"</div>");
+}
+
+for(TaskModel taskModel:byEmployeeProgress) {
+	
+	out.append("<div> byEmployeeProgress:::"+taskModel.toString()+"</div>");
+}
+
+for(TaskModel taskModel:inProgress) {
+	
+	out.append("<div> inProgress:::"+taskModel.toString()+"</div>");
+}
+
+for(TaskModel taskModel:all) {
+	
+	out.append("<div> all:::"+taskModel.toString()+"</div>");
+}
+out.append("<div> getTask:::"+getTask.toString()+"</div>");
+
+
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/*try {
+			Context ctx = new InitialContext();
+			 DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/postgres");
+			 System.out.print(ds.toString());
+			 Connection conn = ds.getConnection();
+			 Statement stmt = conn.createStatement();
+             ResultSet rst = stmt.executeQuery("select * from public.task ;");
+             if(rst.next()) {
+         	    out.print("<span>"+rst.getString("description")+"</span>");
+         	    }
+             conn.close();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/*String url,user,passwordu,z;
+        url="jdbc:postgresql://localhost:5432/EmployeeManagment";
+     user="postgres";
+    passwordu="123456";
+    Connection con;
+   
+	try {
+        Class.forName("org.postgresql.Driver");
+
+		con = DriverManager.getConnection(url, user, passwordu);
+		z="select * from public.task ;";
+		Statement stmt=con.createStatement();
+	    ResultSet rs= stmt.executeQuery(z);
+	    if(rs.next()) {
+	    out.print("<span>"+rs.getString("description")+"</span>");
+	    }
+	    con.close();
+	}catch (SQLException | ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+
+		//JOptionPane.showMessageDialog(null,"user name existed");
+		
+
+   }*/ 
+		
+	/*
 		 PrintWriter out = response.getWriter();
 		
 		 double x,y;
@@ -79,7 +213,7 @@ public class Calculater extends HttpServlet {
 				 out.println("<br> invalid inputs " );
 				 
 			 }
-		 }
+		 }*/
 		 
 		 
 	}
