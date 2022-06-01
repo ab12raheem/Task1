1dccf24243d754879c999f5cf12f7224c203e341
ib123 <ib123raheem@gmail.com>
2022-06-01 13:30:53 +0300

    taskDao update

--- FirstApp/src/main/java/dao/TaskDao.java
+++ FirstApp/src/main/java/dao/TaskDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,26 +15,31 @@ import example.model.TaskModel;
 public class TaskDao {
 	
 	private final EmployeeDao employeeDao = new EmployeeDao();
+	private PreparedStatement statement;
+	private final String ID="id",ATTACHMENT="attachment",DESCRIPTION="description",STATUS="status";
+	public TaskModel mapRecordToTask(Integer id, String attachment , String description , String status) {
+		TaskModel task=new TaskModel();
+		 
+		 task.setId(id);
+		 task.setAttachment(attachment);
+		 task.setDescription(description);
+		 task.setStatus(status);
+		 return task;
+	}
 	
 	public Set<TaskModel> getTasks() throws SQLException{
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
-		 String z="select * from task;";
-		 ResultSet rs=statement.executeQuery(z);
+		String z="select * from task;";
+		statement = conn.prepareStatement(z);
+		 
+		 ResultSet rs=statement.executeQuery();
 		 Set<TaskModel> tasks=new HashSet();
 		
 		 
 		 
 		 while(rs.next()) {
-			 TaskModel task=new TaskModel();
-			 
-			 task.setId(rs.getInt("id"));
-			 task.setAttachment(rs.getString("attachment"));
-			 task.setDescription(rs.getString("description"));
-			 task.setStatus(rs.getString("status"));
-			
-			 
+			 TaskModel task=mapRecordToTask(rs.getInt(ID), rs.getString(ATTACHMENT),rs.getString(DESCRIPTION),rs.getString(STATUS));
+				 
 			 tasks.add(task);
 			 
 			 
@@ -42,7 +48,7 @@ public class TaskDao {
 			 throw new IllegalStateException("tasks not found");
 		 }
 		
-		conn.close();
+
 		return tasks;
 		
 		
@@ -52,19 +58,16 @@ public class TaskDao {
 	public Set<TaskModel> getTaskByEmployee(String userName) throws SQLException{
 		EmployeeModel employee=employeeDao.getByUserName(userName);
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
-		 String z="select * from task where id in(select task_id from task_enrolled where employee_id="+employee.getId()+");";
-		 ResultSet rs=statement.executeQuery(z);
+		
+		
+		 String z="select * from task where id in(select task_id from task_enrolled where employee_id=?)";
+		 
+		 statement = conn.prepareStatement(z);
+		 statement.setInt(1, employee.getId());
+		 ResultSet rs=statement.executeQuery();
 		 Set<TaskModel> tasks=new HashSet();
 		 while(rs.next()) {
-			 TaskModel task=new TaskModel();
-			 
-			 task.setId(rs.getInt("id"));
-			 task.setAttachment(rs.getString("attachment"));
-			 task.setDescription(rs.getString("description"));
-			 task.setStatus(rs.getString("status"));
-			
+			 TaskModel task=mapRecordToTask(rs.getInt(ID), rs.getString(ATTACHMENT),rs.getString(DESCRIPTION),rs.getString(STATUS));
 			 
 			 tasks.add(task);
 			 
@@ -74,32 +77,29 @@ public class TaskDao {
 			 throw new IllegalStateException("tasks not found");
 		 }
 		
-		conn.close();
+
 		return tasks;
 	}
 	
 	
-	public Set<TaskModel> getTaskInProgress() throws SQLException{
+	/*public Set<TaskModel> getTaskInProgress() throws SQLException{
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
+
+		
 		String status="inProgress";
-		 String z="select * from task where status='"+status+"';";
-		 ResultSet rs=statement.executeQuery(z);
+		 String z="select * from task where status=?";
+		 statement = conn.prepareStatement(z);
+		 statement.setString(1, status);
+		 ResultSet rs=statement.executeQuery();
 		 Set<TaskModel> tasks=new HashSet();
 		
 		 
 		 
 		 while(rs.next()) {
-			 TaskModel task=new TaskModel();
-			 
-			 task.setId(rs.getInt("id"));
-			 task.setAttachment(rs.getString("attachment"));
-			 task.setDescription(rs.getString("description"));
-			 task.setStatus(rs.getString("status"));
-			
+			 TaskModel task=mapRecordToTask(rs.getInt(ID), rs.getString(ATTACHMENT),rs.getString(DESCRIPTION),rs.getString(STATUS));
 			 
 			 tasks.add(task);
+			 
 			 
 			 
 		 }
@@ -107,7 +107,7 @@ public class TaskDao {
 			 throw new IllegalStateException("tasks not found");
 		 }
 		
-		conn.close();
+		
 		return tasks;
 		
 		
@@ -146,27 +146,56 @@ public class TaskDao {
 		
 		
 		
-	}
-	public Set<TaskModel> getTaskInProgressByEmployee(String userName) throws SQLException{
+	}*/
+	
+	public Set<TaskModel> getTaskByStatus(String status) throws SQLException{
+	Connection conn=MyConnection.getConnection();
+	 String z="select * from task where status=?";
+	 statement = conn.prepareStatement(z);
+	 statement.setString(1, status);
+	 ResultSet rs=statement.executeQuery();
+	 Set<TaskModel> tasks=new HashSet();
+	
+	 
+	 
+	 while(rs.next()) {
+		 TaskModel task=mapRecordToTask(rs.getInt(ID), rs.getString(ATTACHMENT),rs.getString(DESCRIPTION),rs.getString(STATUS));
+		 
+		 tasks.add(task);
+		 
+		 
+		 
+	 }
+	 if(tasks.isEmpty()) {
+		 throw new IllegalStateException("tasks not found");
+	 }
+	
+	
+	return tasks;
+	
+	
+}
+	
+	
+	
+	
+	public Set<TaskModel> getTaskByStatusAndEmployee(String userName,String status) throws SQLException{
 		EmployeeModel employee=employeeDao.getByUserName(userName);
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
-		String status="inProgress";
-		 String z="select * from task where status='"+status+"' and id in(select task_id from task_enrolled where employee_id="+employee.getId()+") ;";
-		 ResultSet rs=statement.executeQuery(z);
+	
+		
+	
+		 String z="select * from task where status=? and id in(select task_id from task_enrolled where employee_id=?) ";
+		 statement = conn.prepareStatement(z);
+		 statement.setString(1, status);
+		 statement.setInt(2, employee.getId());
+		 ResultSet rs=statement.executeQuery();
 		 Set<TaskModel> tasks=new HashSet();
 		
 		 
 		 
 		 while(rs.next()) {
-			 TaskModel task=new TaskModel();
-			 
-			 task.setId(rs.getInt("id"));
-			 task.setAttachment(rs.getString("attachment"));
-			 task.setDescription(rs.getString("description"));
-			 task.setStatus(rs.getString("status"));
-			
+			 TaskModel task=mapRecordToTask(rs.getInt(ID), rs.getString(ATTACHMENT),rs.getString(DESCRIPTION),rs.getString(STATUS));
 			 
 			 tasks.add(task);
 			 
@@ -176,10 +205,10 @@ public class TaskDao {
 			 throw new IllegalStateException("tasks not found");
 		 }
 		
-		conn.close();
+
 		return tasks;
 	}
-	public Set<TaskModel>getTaskDoneByEmployee(String userName) throws SQLException{
+	/*public Set<TaskModel>getTaskDoneByEmployee(String userName) throws SQLException{
 		EmployeeModel employee=employeeDao.getByUserName(userName);
 		Connection conn=MyConnection.getConnection();
 		Statement statement;
@@ -210,23 +239,22 @@ public class TaskDao {
 		
 		conn.close();
 		return tasks;
-	}
+	}*/
 	public TaskModel getById(Integer id) throws SQLException {
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
-		 String z="select * from task where id="+id+";";
-		 ResultSet rs=statement.executeQuery(z);
+		
+		
+		 String z="select * from task where id=?";
+		 statement = conn.prepareStatement(z);
+		 statement.setInt(1, id);
+		 ResultSet rs=statement.executeQuery();
 		
 		 
 		 TaskModel task=new TaskModel();
 		 
 		if(rs.next()) {
 			 
-			task.setId(rs.getInt("id"));
-			 task.setAttachment(rs.getString("attachment"));
-			 task.setDescription(rs.getString("description"));
-			 task.setStatus(rs.getString("status"));
+			task=mapRecordToTask(rs.getInt(ID), rs.getString(ATTACHMENT),rs.getString(DESCRIPTION),rs.getString(STATUS));
 				return task;
 			 
 		 }
@@ -235,16 +263,14 @@ public class TaskDao {
 	public void addTask (TaskModel task) throws SQLException {
 		
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
-		statement.executeUpdate("INSERT INTO task"
+		String z="INSERT INTO task"
 				+ " (id,description, attachment, status) " 
-				+ "VALUES (DEFAULT, '"+task.getDescription()+"',"
-						+ " '"+task.getAttachment()+"', '"+task.getStatus()+"')");
-		
-		conn.close();
-		
-		
+				+ "VALUES (DEFAULT,?,?,?)";
+		statement=conn.prepareStatement(z);
+		statement.setString(1, task.getDescription());
+		statement.setString(2, task.getAttachment());
+		statement.setString(3, task.getStatus());
+		statement.executeUpdate();
 		
 		
 	}
@@ -252,11 +278,14 @@ public class TaskDao {
 		EmployeeModel employee=employeeDao.getByUserName(userName);
 		TaskModel task=getById(id);
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
-		statement.executeUpdate("INSERT INTO task_enrolled"
+		String z="INSERT INTO task_enrolled"
 				+ " (id,task_id, employee_id) " 
-				+ "VALUES (DEFAULT, "+task.getId()+","+employee.getId()+")");
+				+ "VALUES (DEFAULT,?,?)";
+
+		statement = conn.prepareStatement(z);
+		statement.setInt(1, task.getId());
+		statement.setInt(1, employee.getId());
+		statement.executeUpdate();
 	}
 	public void deleteTask(Integer id) {
 		
@@ -264,15 +293,20 @@ public class TaskDao {
 	public void updateTask(TaskModel task,Integer id) throws SQLException {
 		TaskModel task1=getById(id);
 		Connection conn=MyConnection.getConnection();
-		Statement statement;
-		statement = conn.createStatement();
-		String s="UPDATE task SET description ='"+task.getDescription()+"', "
-				+ "attachment='"+task.getAttachment()+"',"
-						+ "status='"+task.getStatus()+"' WHERE id="+id+"";
+		
+		
+		String s="UPDATE task SET description =?, "
+				+ "attachment=?,"
+						+ "status=? WHERE id=?";
+		statement = conn.prepareStatement(s);
+		statement.setString(1,task.getDescription());
+		statement.setString(2,task.getAttachment());
+		statement.setString(3,task.getStatus());
+		statement.setInt(4,id);
 	
 		
 		statement.executeUpdate(s);
-		conn.close();
+	
 		
 		
 	}
